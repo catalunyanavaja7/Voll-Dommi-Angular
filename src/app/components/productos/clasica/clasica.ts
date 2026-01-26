3cc7fa65b59a7a0b0a67b160c5cedecff1d179fa
asuraxxd <ahiram.rivas@institutvidreres.cat>
2026-01-26 18:51:56 +0100

    pop up que impide la compra sin inicio de sesion

--- src/app/components/productos/clasica/clasica.ts
+++ src/app/components/productos/clasica/clasica.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import {UserAuth} from '../../../authz/userAuth/user-auth';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Producto } from '../../../models/producto';
 import { Carrito } from '../../../services/carrito';
-import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-clasica',
-  imports: [FormsModule, RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule,FormsModule],
   templateUrl: './clasica.html',
   styleUrls: ['./clasica.css'],
-  standalone: true,
 })
 export class Clasica {
   productos: Producto[] = [
@@ -18,20 +19,52 @@ export class Clasica {
       id: '1',
       nombre: 'Voll - Dommi Original Pack (6 x33cl Latas)',
       precio: 8.70,
-      imagen: 'src/assets/images/clasica.png',
+      imagen: 'assets/images/clasica.png',
     },
     {
       id: '2',
       nombre: 'Caja (12 x25cl Botellas)',
       precio: 12.70,
-      imagen: 'src/assets/images/clasica.png',
+      imagen: 'assets/images/clasica.png',
     }
   ];
 
   tipoSeleccionado: string = '1'; // id del producto seleccionado
   cantidad: number = 1;
 
-  constructor(private carrito: Carrito) {}
+  mostrarPopup: boolean = false;
+
+  constructor(
+    private authService: UserAuth,
+    private router: Router,
+    private carrito: Carrito
+  ) {}
+
+  // Verificar si está logeado
+  verificarYAnadir(): void {
+    if (!this.authService.estaLogueado()) {
+      this.mostrarPopup = true;
+    } else {
+      this.agregarAlCarrito();
+    }
+  }
+
+  // Cerrar el popup
+  cerrarPopup(): void {
+    this.mostrarPopup = false;
+  }
+
+  // Ir a la página de registro
+  irARegistro(): void {
+    this.mostrarPopup = false;
+    this.router.navigate(['/registro']); // Ajusta la ruta según tu routing
+  }
+
+  // Ir a la página de login
+  irALogin(): void {
+    this.mostrarPopup = false;
+    this.router.navigate(['/sesion']); // Ajusta la ruta según tu routing
+  }
 
   agregarAlCarrito() {
     const producto = this.productos.find(p => p.id === this.tipoSeleccionado);
