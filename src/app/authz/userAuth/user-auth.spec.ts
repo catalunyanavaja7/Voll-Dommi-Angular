c2725c7efca30e5ec904ad80702b268e2147c9aa
agustin <ruizsosaagustin@institutvidreres.cat>
2026-03-16 11:06:54 +0100

    Commit de specs

--- src/app/authz/userAuth/user-auth.spec.ts
+++ src/app/authz/userAuth/user-auth.spec.ts
@@ -1,16 +1,35 @@
-import { TestBed } from '@angular/core/testing';
 
-import { UserAuth } from './user-auth';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { UserAuth } from '../userAuth/user-auth';
 
 describe('UserAuth', () => {
   let service: UserAuth;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        UserAuth,
+        { provide: Router, useValue: { navigate: () => {} } }
+      ]
+    });
     service = TestBed.inject(UserAuth);
   });
 
-  it('should be created', () => {
+  it('debe crear el servicio', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('debe retornar false si no hay token', () => {
+    sessionStorage.clear();
+    expect(service.estaLogueado()).toBe(false);
+  });
+
+  it('debe retornar true si hay token', () => {
+    sessionStorage.setItem('token', 'test');
+    expect(service.estaLogueado()).toBe(true);
+    sessionStorage.clear();
   });
 });
